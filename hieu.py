435549cf7f1bf1da4c1382028e8955fd7b65443d
Vicky522 <vothanhvan522@gmail.com>
2021-04-05 00:21:51 +0700

    feat:tro ly ao

---
+++ hieu.py
@@ -0,0 +1,10 @@
+
+if you =="":
+    robot_brain = "I can't hear you, you try again"
+elif you =="hello":
+    robot_brain='Hello Van'
+elif you == "today":
+    robot_brain="sunday"
+else: robot_brain ="I'm fine, thank you and you"
+
+print(robot_brain)
